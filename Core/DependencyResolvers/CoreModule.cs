72cf62016a5ede1ac66d122402fdc4433f8fec16
Murat Topallar <topallarm@outlook.com>
2021-11-02 08:59:55 +0300

    Notlarda iyileştirmeler yapıldı.

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -11,11 +11,11 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Core.DependencyResolvers
 {
-    public class CoreModule : ICoreModule //burada proje bağımsız bağımlılıklarımızı çözüyoruz.Proje bağımlı olan bağımlılıklar business altındaki dependency resolver klasörü içinde çözüldü.
+    public class CoreModule : ICoreModule //burada proje bağımsız bağımlılıklarımızı çözüyoruz.Proje bağımlı olan bağımlılıklar business altındaki DependencyResolvers klasörü içinde çözüldü.
     {
         public void Load(IServiceCollection serviceCollection)
         {
-            serviceCollection.AddMemoryCache(); //IMemoryCache injection ının çalışabilmesi gerekli.
+            serviceCollection.AddMemoryCache(); //IMemoryCache injection ının (yani MemoryCacheManager sınıfının içindeki IMemoryCache _memoryCache in) çalışabilmesi için gerekli. IMemoryCache de buradaki servicesCollection.AddMemoryCache de Microsoftun kendi sınıfları. Yani startup dan direk injection yapabiliriz ancak aspect olarak yazdığımız için API den daha geride yapıyoruz injectionlarımızı. serviceCollection.AddMemoryCache() Microsoft'un built in cache manajerinin devreye alınması için gerekli.
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
             serviceCollection.AddSingleton<Stopwatch>(); //kronometre enjeksiyonu.
